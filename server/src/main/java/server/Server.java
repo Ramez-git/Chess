063337cb8bfab20cc83e80b5e6634b01b8087e7c
Ramez <Ramez.gammoh.2004@gmail.com>
2024-02-25 19:37:55 -0700

    added all endpoints without functionallity

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,6 @@
 package server;
 
+import com.google.gson.Gson;
 import spark.*;
 
 import java.lang.module.ResolutionException;
@@ -17,6 +18,12 @@ public class Server {
         Spark.init();
         Spark.awaitInitialization();
         Spark.post("/user",this::addUser);
+        Spark.post("/session",this::loginUser);
+        Spark.post("/game",this::createGame);
+        Spark.delete("/session", this::deleteSession);
+        Spark.delete("/db", this::deleteEVERYTHING);
+        Spark.put("/game",this::joinGame);
+        Spark.get("/game", this::listGames);
         return Spark.port();
     }
 
@@ -26,7 +33,24 @@ public class Server {
     }
 
     private Object addUser(Request req,Response res) throws ResolutionException{
-        return "acknowledged";
+        return new Gson().toJson("acknowledged addUser");
     }
-
+    private Object loginUser(Request req, Response res) throws ResolutionException{
+        return new Gson().toJson("acknowledged loginUser");
+    }
+    private Object deleteSession(Request req, Response res) throws ResolutionException{
+        return new Gson().toJson("acknowledged deleteSession");
+    }
+    private Object listGames(Request req, Response res) throws ResolutionException{
+        return new Gson().toJson("acknowledged listGame");
+    }
+    private Object createGame(Request req, Response res) throws ResolutionException{
+        return new Gson().toJson("acknowledged createGame");
+    }
+    private Object joinGame(Request req, Response res) throws ResolutionException{
+        return new Gson().toJson("acknowledged joinGame");
+    }
+    private Object deleteEVERYTHING(Request req, Response res) throws ResolutionException{
+        return new Gson().toJson("acknowledged deleteEVERYTHING");
+    }
 }
