501e1fc9c3cc84795d67442706696e94b4be1c9b
Ramez <Ramez.gammoh.2004@gmail.com>
2024-02-28 23:05:34 -0700

    added some tests

---
+++ server/src/test/java/serviceTests.java
@@ -0,0 +1,86 @@
+import dataAccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+import chess.ChessGame;
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class serviceTests {
+
+    private UserService userService;
+    private GameService gameService;
+    private AuthService authService;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testUserService_CreateUser() {
+
+    }
+
+    @Test
+    public void testUserService_Login() {
+
+    }
+
+    @Test
+    public void testUserService_DeleteAll() {
+
+    }
+
+    @Test
+    public void testGameService_ListGames() {
+
+    }
+
+    @Test
+    public void testGameService_GetGame() {
+
+    }
+
+    @Test
+    public void testGameService_UpdateGame() {
+
+    }
+
+    @Test
+    public void testGameService_CreateGame() {
+
+    }
+
+    @Test
+    public void testGameService_DeleteAll() {
+
+    }
+
+    @Test
+    public void testAuthService_GetAuth() {
+
+    }
+
+    @Test
+    public void testAuthService_Getusr() {
+
+    }
+
+    @Test
+    public void testAuthService_DeleteAll() {
+
+    }
+
+    @Test
+    public void testAuthService_DeleteSession() {
+
+    }
+
+    @Test
+    public void testAuthService_CreateAuth() {
+
+    }
+}
